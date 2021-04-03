afaaa55961c4f4121fcd380445c067492b03240b
updown <updown-kurly@kurlycorp.com>
2021-04-03 23:06:36 +0900

    주석 추가

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,15 +6,15 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
-public class OrderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     // 고정할인금액와 비율할인정책을 변경하려면 아래 구현객체만 변경하면된다
     // 아래 둘 다 OCP, DIP 위반
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-//    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 이런식으로 DiscountPolicy 인터페이스는 본인이 어떤 구현체를 사용할지 까지는 안해야 관심사의 분리가 된다 OCP, DIP
+//    private DiscountPolicy discountPolicy; // OCP 통과
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
